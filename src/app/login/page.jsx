aec14203b44f2e0a00b2153128b1b6948da89114
Ashish48Maurya <maurya.48.ashish.com>
2024-06-12 22:06:17 +0530

    delete register folder

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -7,9 +7,9 @@ import { useRouter } from 'next/navigation';
 import { useAuth } from '../components/Client';
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const router = useRouter();
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  // const router = useRouter();
   const {signInWithGoogle} = useAuth();
 
   const handleLogin = async (e) => {
@@ -24,25 +24,7 @@ const Login = () => {
         <title>Login</title>
       </Head>
       <h2 style={{ marginBottom: "10px" }}>Login</h2>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          style={{ marginBottom: '10px', padding: '8px', width: '100%' }}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          style={{ marginBottom: '10px', padding: '8px', width: '100%' }}
-        />
-        <button style={{ width: '100%', padding: '10px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer', marginBottom: "10px" }} type='submit'>Login</button>
-        <button style={{ width: '100%', padding: '10px', backgroundColor: 'blueviolet', color: "white", border: 'none', borderRadius: '5px', cursor: 'pointer', marginBottom: "10px" }} type='submit'>SignIn with Google</button>
-        <Link href='/register'>Sign Up</Link>
-      </form>
+        <button onClick={handleLogin} style={{ width: '100%', padding: '10px', backgroundColor: 'blueviolet', color: "white", border: 'none', borderRadius: '5px', cursor: 'pointer', marginBottom: "10px" }} type='submit'>SignIn with Google</button>
     </div>
   );
 };
